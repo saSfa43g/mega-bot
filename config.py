6bd899e37304b467f6fa0bbb45f8ff7557783d1f
‪عبد الرحيم‬‏ <157739294+saSfa43g@users.noreply.github.com>
2025-04-18 22:46:25 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    API_ID = 27978622          # استبدل بـ API ID من my.telegram.org
+    API_HASH = "2f50c722e9885a80648b8466528bb462"  # استبدل بالـ API HASH
+    BOT_TOKEN = "7619304970:AAFv4wJ0-xDqJi8PQvfS1PSIQEwEGUoed4s"
+    
+    MEGA_EMAIL = "7faf3ccf75@emaily.pro"
+    MEGA_PASSWORD = "7faf3ccf75"
